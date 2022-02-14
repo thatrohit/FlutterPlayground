c1adfd0acd0068c0667c1b6bf5c073714eb905a9
Rohit Gupta <rohit.gupta1@usbank.com>
2022-02-14 09:02:01 +0530

    Updated code for scaffold exercise

--- lib/first.dart
+++ lib/first.dart
@@ -1,14 +1,1 @@
-void main(List<String> arguments) {
-  var obj = MyClass();
-  obj.printClass();
-}
 
-class MyClass {
-  int age = 4;
-  String name = "Rohit";
-  String printClass() {
-    return "$name is $age years old";
-  }
-
-  MyClass();
-}
